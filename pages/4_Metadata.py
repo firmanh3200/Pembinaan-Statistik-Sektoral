34a4af9d4e45c77bb0fb85a09503ad8c2e9e3df4
firmanh3200 <olah3273@gmail.com>
2024-11-08 14:39:56 +0700

    apdet konten kosong

--- pages/4_Metadata.py
+++ pages/4_Metadata.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+st.set_page_config(layout='wide')
+
+st.title(':green[SATU DATA INDONESIA]')
+
+st.subheader('METADATA STATISTIK')
+st.success('Berdasarkan Sistem Rujukan Statistik')
+
+st.link_button("Metadata Statistik Dasar", url="https://sirusa.bps.go.id/sirusa/index.php/dasar/index")
+
+st.link_button("Metadata Statistik Sektoral", url="https://sirusa.bps.go.id/sirusa/index.php/sektoral/index")
+
+st.link_button("Metadata Statistik Khusus", url="https://sirusa.bps.go.id/sirusa/index.php/khusus/index")
+
+st.link_button("Glosarium", url="https://sirusa.bps.go.id/sirusa/index.php/istilah/index")
+
+st.link_button("Tata Cara Pelaksanaan Survei", url="https://sirusa.bps.go.id/sirusa/index.php/solusi/page?view=konsep&tab=1")
