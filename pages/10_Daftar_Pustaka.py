35c3425daf3390596b87d6e9bd9148b82e87a6b1
firmanh3200 <olah3273@gmail.com>
2025-01-06 08:29:07 +0700

    menambah menu daftar pustaka

---
+++ pages/10_Daftar_Pustaka.py
@@ -0,0 +1,59 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+st.header('Daftar Pustaka', divider='rainbow')
+
+st.write('1.	Undang-Undang Republik Indonesia Nomor 23 Tahun 2014 tentang Pemerintahan Daerah;')
+st.write('2.	Undang-Undang Republik Indonesia Nomor 16 Tahun 1997 tentang Statistik;')
+st.write('3.	Peraturan Pemerintah Nomor 51 Tahun 1999 tentang Penyelenggaraan Statistik;')
+st.write('4.	Peraturan Pemerintah Nomor 13 Tahun 2019 tentang Laporan dan Evaluasi \
+    Penyelenggaraan Pemerintah Daerah;')
+st.write('5.	Peraturan Pemerintah Nomor 13 Tahun 2024 tentang Jenis dan Tarif atas \
+    Jenis Penerimaan Negara Bukan Pajak yang Berlaku pada Badan Pusat Statistik;')
+st.write('6.	Peraturan Presiden Nomor 39 Tahun 2019 tentang Satu Data Indonesia;')
+st.write('7.	Peraturan Presiden Nomor 95 Tahun 2018 tentang Sistem Pemerintahan Berbasis Elektronik;')
+st.write('8.	Peraturan Badan Pusat Statistik Nomor 2 Tahun 2019 tentang Persyaratan \
+    dan Tata Cara Pengenaan Tarif Rp0,00 (Nol Rupiah) Terhadap Pihak Tertentu \
+        atas Penerimaan Negara Bukan Pajak yang Berlaku pada Badan Pusat Statistik;')
+st.write('9.	Peraturan Badan Pusat Statistik Nomor 4 Tahun 2019 tentang Norma, Standar, \
+    Prosedur, dan Kriteria Penyelenggaraan Statistik Sektoral oleh Pemerintah Daerah;')
+st.write('10.	Peraturan Badan Pusat Statistik Nomor 4 Tahun 2020 tentang Petunjuk Teknis Standar Data Statistik;')
+st.write('11.	Peraturan Badan Pusat Statistik Nomor 5 Tahun 2020 tentang Petunjuk Teknis Metadata Statistik;')
+st.write('12.	Peraturan Badan Pusat Statistik Nomor 3 Tahun 2022 tentang Evaluasi Penyelenggaraan Statistik Sektoral;')
+st.write('13.	Peraturan Menteri Komunikasi dan Informatika Nomor 1 Tahun 2023 tentang \
+    Interoperabilitas Data dalam Penyelenggaraan Sistem Pemerintahan Berbasis \
+        Elektronik dan Satu Data Indonesia;')
+st.write('14.	Peraturan Menteri Komunikasi dan Informatika Nomor 20 Tahun 2016 tentang \
+    Perlindungan Data Pribadi dalam Sistem Elektronik;')
+st.write('15.	Keputusan Menteri Perdagangan Nomor 1544 Tahun 2024 tentang Pedoman Penyelenggaraan \
+    Statistik Sektoral di Kementerian Perdagangan')
+st.write('16. Petunjuk Pelaksanaan dan Petunjuk Teknis Penyelenggaraan Statistik Sektoral Kabupaten Ciamis')
+st.write('17. Pedoman Penyelenggaraan Statistik Sektoral Kementerian Pariwisata dan Ekonomi Kreatif')
+st.write('18. Pedoman Penyelenggaraan Statistik Sektoral di Lingkup Pemerintah Kota Malang')
+st.write('19. Pedoman Penyelenggaraan Statistik Sektoral di Lingkungan Lembaga Kebijakan Pengadaan \
+    Barang/Jasa Pemerintah')
+st.write('20. Pedoman Penyelenggaraan Statistik Lingkungan Hidup dan Kehutanan')
+st.write('21. Panduan Operasional Penghimpunan dan Pengelolaan Metadata Kegiatan Statistik Sektoral/Khusus. \
+    https://drive.google.com/file/d/13EmZR8Z9Tp3yEa71iTZKz-KSrE9bKtFr/view?usp=sharing')
+st.write('22. Peraturan Gubernur Jawa Barat Nomor 40 Tahun 2023 Tentang SATU DATA TERPADU KELUARGA JAWA BARAT')
+st.write('23. Pedoman Evaluasi Penyelenggaraan Statistik Sektoral. \
+    https://drive.google.com/file/d/1B37k_35MENcx6MQ5UWmnV-EleC-derH0/view?usp=sharing')
+st.write('24. Peraturan Gubernur Jawa Barat Nomor 47 Tahun 2022 Tentang PERATURAN PELAKSANAAN SATU DATA JAWA BARAT')
+st.write('25. Peraturan Daerah Provinsi Jawa Barat Nomor 24 Tahun 2012 Tentang SATU DATA PEMBANGUNAN JAWA BARAT')
+st.write('26. Peraturan Daerah Provinsi Jawa Barat Nomor 4 Tahun 2021 Tentang PENYELENGGARAAN KOMUNIKASI DAN INFORMATIKA, STATISTIK, DAN PERSANDIAN')
+st.write('27. Buku Pedoman Penyelenggaraan Statistik Sektoral Indramayu')
+st.write('28. Panduan Penyusunan Metadata Statistik Ketahanan Pangan 2021, \
+    Badan Ketahanan Pangan Kementerian Pertanian')
+st.write('29. https://sipd.go.id/ewalidata/54050dab11e0f218cf632ec8478dbc71755a65e7/?m=public_dssd_tabel')
+st.write('30. https://jdih.jabarprov.go.id/page/produk_hukum/12/')
+st.write('31. https://jdih.web.bps.go.id/public/index.php')
+
+# Menambahkan widget Galichat
+chat_script = """
+    <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
+    <script src="https://files.bpcontent.cloud/2025/01/04/03/20250104035620-PTG96A7D.js"></script>
+"""
+st.components.v1.html(chat_script)
+
+st.subheader("", divider='green')
